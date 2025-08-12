209e7c6b30572db2c470e97455962ae91b82727f
shail-712 <shailshajim@gmail.com>
2025-08-12 15:51:10 +0530

    Add dataset loading and preprocessing step

---
+++ credit_card_fraud_detection/scripts/preprocessing.py
@@ -0,0 +1,16 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def preprocess_data(df):
+    X = df.drop("Class", axis=1)
+    y = df["Class"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
+
+    scaler = StandardScaler()
+    X_train[["Time", "Amount"]] = scaler.fit_transform(X_train[["Time", "Amount"]])
+    X_test[["Time", "Amount"]] = scaler.transform(X_test[["Time", "Amount"]])
+
+    return X_train, X_test, y_train, y_test
